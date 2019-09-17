37beada4e48ab76fa79d0f2bc564a8316dc5a29e
Brian <besont40@harrisonburg.k12.va.us>
2019-09-17 09:13:35 -0400

    hi

--- game.js
+++ game.js
@@ -2,13 +2,25 @@ var score = 0;
 
 postToPage(score); // Print to the page
 
-createButton('Button', btnPress);
+createButton('grow', btnPress);
 changeTitle('Clicker Game');
 
-setInterval(btnPress, 1000);
+setInterval(btnPress, 2000);
 
 function btnPress() {
   score++;
   postToPage(score);
 }
+postToPage(score); // Print to the page
+
+createButton('decreace', btnPress);
+changeTitle('Clicker Game');
+
+setInterval(btnPress, -1000);
+
+function btnPress() {
+  score--;
+  postToPage(score);
+}
+
 
