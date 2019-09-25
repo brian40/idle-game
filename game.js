164449ab8db117596d61bee0fd8ae02ef67e03a3
Brian <besont40@harrisonburg.k12.va.us>
2019-09-25 09:45:12 -0400

    Update game.js

--- game.js
+++ game.js
@@ -1,13 +1,12 @@
 var title = new Title('Welcome to the game')
-let score = 0;
-let button1 = new Button('OH HI MARK');
-let button2 = new Button('GROW'); 
-let button3 = new Button('DECREASE');
+var score = 0;
+var button1 = new Button('OH HI MARK');
+var button2 = new Button('GROW'); 
+var button3 = new Button('DECREASE');
+var scr = new Text(score);                    
 
-let scr = new Text(score);
+
 changeFont('Impact');
-
-button.hide()
 
 setInterval(scoreIncrease, 10)
 
@@ -22,9 +21,8 @@ var title = new Title('Welcome to the Game'); // Create a title element
 var button = new Button('hello', btnPress);   // Create a button element
 var hidden = new Button('Boo!');
 //hidden.hide();
-var scr = new Text(score);                    // Create a text element
 //changes
-setInterval(btnPress, 1000);   // Set up a loop
+   // Set up a loop
 
 function btnPress() {
   score++;
